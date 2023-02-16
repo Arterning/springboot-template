234b92332b7bafc16b333420f3a5abb632b1fe18
Arterning <ning.huang.ridson@gmail.com>
2023-02-16 11:03:55 +0800

    Add config file

---
+++ src/main/java/cn/ning/springboot/starter/properties/DemoConfig.java
@@ -0,0 +1,31 @@
+package cn.ning.springboot.starter.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 读取配置文件中的信息
+ * @author LinkinStar
+ */
+@Component
+@ConfigurationProperties(prefix = "demo")
+public class DemoConfig {
+    private int id;
+    private String val;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+}
